292c94c59ffd4118349fe8c1ab20a0a58983dfbf
Kristiina <kristiina@gmail.com>
2025-09-18 15:39:36 +0300

    WIP

---
+++ src/views/LoginView.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>
+    Tere, palun logi sisse
+  </h1>
+
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        Column
+      </div>
+      <div class="col">
+        Column
+      </div>
+      <div class="col">
+        Column
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginView',
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {
+  }
+}
+</script>
