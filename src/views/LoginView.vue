4a8a7eb587ae60841e9b188441506b6413e5af03
Kristiina <kristiina@gmail.com>
2025-09-18 16:26:35 +0300

    WIP

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -6,36 +6,51 @@
   <div class="container text-center justify-content-center">
     <div class="row">
       <div class="col">
-
       </div>
       <div class="col">
         <form>
           <div class="mb-3">
             <label class="form-label">Kasutajanimi</label>
-            <input class="form-control">
+            <input v-model="username" class="form-control">
           </div>
           <div class="mb-3">
             <label class="form-label">Password</label>
-            <input class="form-control">
+            <input v-model="password" class="form-control">
           </div>
-          <button type="submit" class="btn btn-primary">Sisene</button>
+          <button @click="login" type="submit" class="btn btn-primary">Sisene</button>
         </form>
       </div>
       <div class="col">
-
       </div>
-
     </div>
   </div>
 </template>
 
 <script>
+import LoginService from "@/services/LoginService";
+
 export default {
   name: 'LoginView',
   data() {
-    return {}
+    return {
+      username: '',
+      password: '',
+    }
   },
-  methods: {},
+  methods: {
+
+    login(username, password) {
+
+      LoginService.sendLoginRequest(username, password)
+          .then()
+          .catch()
+
+
+
+    }
+
+  },
+
   mounted() {
   }
 }
